ae588c71b895313c2d8ecd626d2cfd0b17d5287f
wellingtonhiago <wellingtonhiago2.0@gmail.com>
2023-03-28 01:26:48 -0300

    Refatoração

--- udacityOOPJava/src/main/java/input/WordCountProfissional.java
+++ udacityOOPJava/src/main/java/input/WordCountProfissional.java
@@ -6,20 +6,26 @@ import java.io.IOException;
 
 public class WordCountProfissional {
     public static void main(String[] args) {
+        // Nome do arquivo a ser lido
         String fileName = "ATaleofTwoCities.txt";
+        // Contador de palavras
         int wordCount = 0;
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
             String line;
+            // Lê cada linha do arquivo
             while ((line = br.readLine()) != null) {
+                // Divide a linha em palavras usando espaços em branco como separador
                 String[] words = line.split("\\s+");
+                // Adiciona o número de palavras na linha ao contador de palavras
                 wordCount += words.length;
             }
         } catch (IOException e) {
+            // Em caso de erro de leitura do arquivo, exibe o stack trace
             e.printStackTrace();
         }
 
+        // Exibe o resultado final
         System.out.println("O arquivo " + fileName + " contém " + wordCount + " palavras.");
-
     }
 }
